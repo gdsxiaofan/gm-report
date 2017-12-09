8f4ec26e31421cbfcd79c51665e8619b7dbe56f5
duhongda <378935454@qq.com>
2017-12-09 16:18:59 +0800

    权限拦截添加对header的支持

--- gm-api/src/main/java/com/huaxia/tongyong/AppStartConfiguration.java
+++ gm-api/src/main/java/com/huaxia/tongyong/AppStartConfiguration.java
@@ -3,7 +3,6 @@
  */
 package com.huaxia.tongyong;
 import lombok.extern.slf4j.Slf4j;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
